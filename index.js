da572876c8baa50882619494c4118fe52440222b
Ola Adedoyin <olalinux03@gmail.com>
2020-01-09 01:00:55 -0500

    parsed request path

--- index.js
+++ index.js
@@ -4,11 +4,24 @@
 
  // Dependencies
  const http = require('http');
+ const url = require ('url'); // library for all things url
 
  // The server should respond to all requests with a string
 // create a server object
 const server = http.createServer((req, res) => {
-    res.end(`Hello World\n`)
+
+    // Get the url and parse it
+    const parsedUrl = url.parse(req.url, true); // true => parse the query string too
+
+    // Get the path from the url
+    const path = parsedUrl.pathname; // untrimmed path
+    const trimmedPath = path.replace(/^\/+|\/+$/g, ''); // remove the slashes from both ends
+
+    // send the response
+    res.end(`Hello World\n`);
+
+    // Log the request path
+    console.log(`Request received on path: `, trimmedPath);
 });
 
  //start the server and have it listen on port 3000
